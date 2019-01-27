af9a800255988102252d98a57131f5bda86b0370
Yash Jain <yashjain.1999@gmail.com>
2019-01-27 20:28:14 +0530

    Basic Settings Fragment added
    A minimalistic settings fragment has been added, along with a working Main Activity ro switch between the two fragment.

--- app/src/main/java/com/macbitsgoa/bitsgridwatch/MainActivity.java
+++ app/src/main/java/com/macbitsgoa/bitsgridwatch/MainActivity.java
@@ -2,14 +2,59 @@ package com.macbitsgoa.bitsgridwatch;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
+    FrameLayout frameLayout;
+    Fragment selectedFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNav = (BottomNavigationView) findViewById(R.id.bottomnav_activity_main);
+        frameLayout = (FrameLayout) findViewById(R.id.framelayout_activity_main);
+
+        bottomNav.setSelectedItemId(R.id.item_bottomnav_home);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item_bottomnav_home:
+                        selectedFragment = HomeFragment.newInstance();
+                        break;
+                    case R.id.item_bottomnav_settings:
+                        selectedFragment = SettingsFragment.newInstance();
+                        break;
+                    default:
+                        selectedFragment = HomeFragment.newInstance();
+                }
+                FragmentTransaction transacion = getSupportFragmentManager().beginTransaction();
+                transacion.replace(R.id.framelayout_activity_main, selectedFragment);
+                transacion.commit();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.framelayout_activity_main, HomeFragment.newInstance());
+        transaction.commit();
     }
 }
