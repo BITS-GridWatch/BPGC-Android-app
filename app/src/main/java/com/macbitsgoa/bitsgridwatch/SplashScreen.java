5216bbdc4964df7a80148b7d99a7e3059701660a
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-09-22 16:06:20 +0530

    splash screen added

---
+++ app/src/main/java/com/macbitsgoa/bitsgridwatch/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.macbitsgoa.bitsgridwatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
+
+
+    }
+
+}
