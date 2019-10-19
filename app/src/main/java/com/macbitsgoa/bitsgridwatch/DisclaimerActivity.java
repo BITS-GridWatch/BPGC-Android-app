4224e35ab04ea7b813181d687c38f772320844d3
Yash Jain <yashjain.1999@gmail.com>
2019-10-19 20:54:15 +0530

    Added Disclaimer activity

---
+++ app/src/main/java/com/macbitsgoa/bitsgridwatch/DisclaimerActivity.java
@@ -0,0 +1,14 @@
+package com.macbitsgoa.bitsgridwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisclaimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disclaimer);
+    }
+}
